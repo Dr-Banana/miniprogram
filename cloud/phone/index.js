224faa1faba95747a2dc48e4394f0604320dcd74
Dr-Banana <77912454+Dr-Banana@users.noreply.github.com>
2022-02-03 20:36:10 -0500

    Add files via upload

---
+++ cloud/phone/index.js
@@ -0,0 +1,13 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  console.log("上传的是",event.cloudID)
+  return await cloud.getOpenData({
+list: [event.cloudID],
+
+  })
+}
